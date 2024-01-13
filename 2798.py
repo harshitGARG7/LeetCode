601ed7fef07610596fb56b411cbc33bb4db3c325
Harshit Garg <harshit20301@iiitd.ac.in>
2024-01-13 23:40:58 +0530

    Create 2798.py

---
+++ 2798.py
@@ -0,0 +1,7 @@
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        answer=0
+        for i in hours:
+            if i>=target:
+                answer+=1
+        return answer
