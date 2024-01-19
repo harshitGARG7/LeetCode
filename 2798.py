7d87c685fda9030f4bfcc59cbf2fd189ea36f09d
Harshit Garg <harshit20301@iiitd.ac.in>
2024-01-19 23:46:00 +0530

    Update 2798.py

--- 2798.py
+++ 2798.py
@@ -5,3 +5,11 @@ class Solution:
             if i>=target:
                 answer+=1
         return answer
+
+class Solution:
+    def numberOfEmployeesWhoMetTarget(self, hours: List[int], target: int) -> int:
+        answer=0
+        for i in hours:
+            if i>=target:
+                answer+=1
+        return answer
