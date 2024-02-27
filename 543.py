d08398c45c6bdc951d5c2e2f22e9e5141f4fefd3
Harshit Garg <harshit20301@iiitd.ac.in>
2024-02-27 22:39:56 +0530

    Create 543.py

---
+++ 543.py
@@ -0,0 +1,13 @@
+class Solution:
+    def diameterOfBinaryTree(self, root: Optional[TreeNode]) -> int:
+        def diameter(node, res):
+            if not node:
+                return 0
+            left = diameter(node.left, res)
+            right = diameter(node.right, res)
+            res[0] = max(res[0], left + right)
+            return max(left, right) + 1
+        res = []
+        res.append(0)
+        diameter(root, res)
+        return res[0]
