8ca70e71738bd332378f57948acced3dec4a7fdd
Harshit Garg <harshit20301@iiitd.ac.in>
2024-03-01 13:58:37 +0530

    Create 2864.py

---
+++ March/2864.py
@@ -0,0 +1,11 @@
+#Day1  : problem solved
+class Solution:
+    def maximumOddBinaryNumber(self, s: str) -> str:
+        for i in s:
+            one=s.count('1')
+            zero=s.count('0')
+        a='1'
+        one-=1
+        a=zero*'0'+a
+        a=one*'1'+a
+        return a
