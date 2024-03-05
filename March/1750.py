a459b7d7b53722a52f198fa21f490a58ef4b4b4a
Harshit Garg <harshit20301@iiitd.ac.in>
2024-03-06 02:00:41 +0530

    Create 1750.py

---
+++ March/1750.py
@@ -0,0 +1,13 @@
+#day6
+class Solution:
+    def minimumLength(self, s: str) -> int:
+        left, right = 0, len(s) - 1
+        
+        while left < right and s[left] == s[right]:
+            char = s[left]
+            while left <= right and s[left] == char:
+                left += 1
+            while right >= left and s[right] == char:
+                right -= 1
+        
+        return right - left + 1
