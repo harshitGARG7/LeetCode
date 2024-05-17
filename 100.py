d44f64c14815d694a6b2fb15c13e438f0abc62ea
Harshit Garg <harshit20301@iiitd.ac.in>
2024-05-17 23:57:36 +0530

    Update 100.py

--- 100.py
+++ 100.py
@@ -5,3 +5,4 @@ class Solution:
         if not p or not q:
             return False
         return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+nsnsnsmmssm
